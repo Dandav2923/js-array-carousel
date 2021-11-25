56593232875214c9fb5e2661f76d2afd3511a301
Dandav2923 <daniele.emili23@gmail.com>
2021-11-25 17:30:36 +0100

    creazione primo ed ultimo elemento

--- js/script.js
+++ js/script.js
@@ -7,8 +7,22 @@ const images = [
 ];
 
 const containerImages = document.querySelector('.row .col-4');
-
+const containerImagesBig = document.querySelector ('.row .col-8');
 for (let index = 0; index < images.length; index++) {
-    let image = `<div class="box-image w-100"> <img class="w-100" src="${images[index]}" alt="">`;
-    containerImages.innerHTML += image;   
+    let image = `<div class="box-image w-100"> <img class="w-100 h-100" src="${images[index]}" alt="">`;
+    let imagesBig = `<img class="w-100 h-100 d-none" src="${images[index]}"alt="">`;
+    containerImages.innerHTML += image;
+    containerImagesBig.innerHTML += imagesBig;   
 }
+
+const imagesBox = document.querySelectorAll('.box-image img');
+const imagesBoxBig = document.querySelectorAll('col-8 img');
+
+imagesBox[0].classList.add('first' ,'active');
+imagesBox[imagesBox.length - 1].classList.add('last');
+
+imagesBoxBig[0].classList.add('first', 'active');
+imagesBoxBig[imagesBoxBig.length - 1].classList.add('last');
+
+const up = document.querySelector('.col-4 .box-up');
+const down =document.querySelector('.col-4 .box-down');
